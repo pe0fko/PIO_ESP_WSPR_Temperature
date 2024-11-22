33715220076389007be8a22a3767bc99eefe6727
Fred <pe0fko@gmail.com>
2024-11-22 10:41:48 +0100

    ok

--- src/ESP_WSPR_TEMPERATURE.cpp
+++ src/ESP_WSPR_TEMPERATURE.cpp
@@ -178,7 +178,7 @@
 #define	SCREEN_HEIGHT	64			// OLED display height, in pixels
 
 #define OLED_RESET     -1 	// Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C	///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32, WRORG
+#define SCREEN_ADDRESS 0x3C	///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306			display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Si5351						si5351;
 JTEncode					wspr;
